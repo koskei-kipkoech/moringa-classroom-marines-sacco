a7b66d937c6e8aceaab9dba8e9921b4670868a91
Sam Tomashi <samtomashikayembi@yahoo.fr>
2024-10-03 10:47:52 +0300

    capture form data

--- src/app.js
+++ src/app.js
@@ -1,34 +1,22 @@
-/**
- Event:
-  1. onClick
-  2. onSubmit
-  3. onContentLoad
- */
-
- /**
-  *  {
-        id: 1,
-        name: "Alice Johnson",
-        accountNumber: "BA123456789",
-        loanAmount: 25000,
-        interestRate: 5.5,
-        loanTerm: 5, // in years
-        loanStatus: "Active",
-        birthday: "1990-05-12", // YYYY-MM-DD format
-        gender: "Female",
-        amountPaid: 5000, // amount paid so far
-    },
-
-    name, 
-    accounNumber,
-    loanAmount,
-    birthday, 
-    gender
-  */
+const bankAccounts = []
 
  function handleOnSubmit(event){
     event.preventDefault()
-    console.log("Submitting")
+    const formData = new FormData(event.target)
+    bankAccounts.push({
+            firstName : formData.get("firstName"),
+            lastName: formData.get("lastName"),
+            loanAmount: formData.get("loanAmount"),
+            birthDay: formData.get("birthday"),
+            gender: formData.get("gender")
+        })
+    console.log(bankAccounts)
+
+   
  }
 
 document.getElementById("frm-new-account").addEventListener('submit', handleOnSubmit)
+
+ 
+
+
